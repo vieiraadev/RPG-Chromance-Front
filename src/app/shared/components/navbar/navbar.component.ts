74eee2387de3160afd4bd448c824eb917db26425
vieiraadev <vieiravitor604@gmail.com>
2025-08-28 19:17:30 +0000

    feat(navbar): adiciona ícone de logout na navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -10,25 +10,24 @@ export interface NavigationItem {
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule], 
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
   @Output() navigationChange = new EventEmitter<string>();
+  @Output() logoutEmitter = new EventEmitter<void>(); 
   @Input() initialActive: string = 'home';
   
   activeIcon: string = 'home';
   
   navigationItems = [
-    { id: 'home', label: 'Home', icon: 'bx-home' },           
-    { id: 'characters', label: 'Meus Personagens', icon: 'bx-group' }, 
-    { id: 'campaigns', label: 'Campanhas', icon: 'bx-map-alt' }, 
-    { id: 'play', label: 'Jogar', icon: 'bx-joystick' },         
-    { id: 'profile', label: 'Perfil', icon: 'bx-user' }          
+    { id: 'home', label: 'Home', icon: 'bx-home' },
+    { id: 'characters', label: 'Meus Personagens', icon: 'bx-group' },
+    { id: 'campaigns', label: 'Campanhas', icon: 'bx-map-alt' },
+    { id: 'play', label: 'Jogar', icon: 'bx-joystick' },
+    { id: 'profile', label: 'Perfil', icon: 'bx-user' }
   ];
-  
-  
 
   ngOnInit(): void {
     this.activeIcon = this.initialActive;
@@ -43,5 +42,9 @@ export class NavbarComponent implements OnInit {
 
   isActive(iconId: string): boolean {
     return this.activeIcon === iconId;
+  }
+
+  logout(): void {
+    this.logoutEmitter.emit();
   }
 }
